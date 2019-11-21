e1eaa373a03459f945a708d628fd765e33746953
Ajschlander <Aschlander21@gmail.com>
2019-11-21 14:27:00 -0600

    Added padding to save div.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Select everything
 const dateElement = document.getElementById('date');
+
+// Set date and time and write it to the header tag
 let dateAndTime = moment().format("dddd, MMMM Do YYYY, h:mm a");
 dateElement.innerHTML = dateAndTime;
+
+// Any time slots that have passed that hour it currently is turn to grey background and make it so they cant edit the content inside
